bcd4fe09f5c949e5c564b81305e339b5c7fd36a8
tjbronx92 <39333780+tjbronx92@users.noreply.github.com>
2023-12-17 14:54:50 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fun With Algos #
+----
+
+My tinkerings with Algorithims utilizing Python and C#.
+
+Selection Sort
+
+References:
+*Grokking Algorithms: An illustrated guide for programmers and other curious people
---
+++ SelectionSort/SelectionSort.py
@@ -0,0 +1,49 @@
+'''Function to RETURN index of the Smallest element in an array'''
+def findSmallest(arr):
+    smallest = arr[0]
+    smallestIndex = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallestIndex = i
+    return smallestIndex
+
+'''Function to RETURN index of the Largest element in an array'''
+def findLargest(arr):
+    largest = arr[0]
+    largestIndex = 0
+    for i in range(1, len(arr)):
+        if arr[i] > largest:
+            largest = arr[i]
+            largestIndex = i
+    return largestIndex
+
+def selectionSortAsc(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+def selectionSortDsc(arr):
+    newArr = []
+    for i in range(len(arr)):
+        largest = findLargest(arr)
+        newArr.append(arr.pop(largest))
+    return newArr
+
+'''Testing Sorting Functions'''
+nums = [36, 99, 7, 1, 1001, 88, 1_718_320_918]
+smallestNumIndex = findSmallest(nums)
+smallestNum = nums[findSmallest(nums)]
+small2Large = selectionSortAsc(nums)
+
+nums = [36, 99, 7, 1, 1001, 88, 1_718_320_918]
+largestNumIndex = findLargest(nums)
+largestNum = nums[findLargest(nums)]
+large2Small = selectionSortDsc(nums)
+
+nums = [36, 99, 7, 1, 1001, 88, 1_718_320_918]
+
+
+
